0d806a0f24c800501d0c91f4fafb7c8fdfa103ed
jiangduo <jiangduowansui@gmail.com>
2015-07-06 14:12:56 +0800

    House Robber

---
+++ src/HouseRobber/HouseRobberSolution.java
@@ -0,0 +1,20 @@
+package HouseRobber;
+
+public class HouseRobberSolution {
+	public int rob(int[] nums) {
+        if(nums.length == 0){
+            return 0;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+        if(nums.length == 2){
+            return Math.max(nums[0],nums[1]);
+        }
+        nums[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < nums.length; i++){
+            nums[i] = ((nums[i-2]+nums[i]) > nums[i-1] ? nums[i-2]+nums[i]:nums[i-1]);
+        }
+        return nums[nums.length-1];
+    }
+}
