3bd2e477063c6c52b3dd45e4818a20244f2d45d3
jiangduo <jiangduowansui@gmail.com>
2015-07-10 17:35:12 +0800

    07/10/2015

---
+++ src/KthSmallestElementinaBST/KthSmallestElementinaBST.java
@@ -0,0 +1,37 @@
+package KthSmallestElementinaBST;
+
+import java.util.Stack;
+
+public class KthSmallestElementinaBST {
+	public int kthSmallest(TreeNode root, int k) {
+		Stack<TreeNode> st = new Stack<>();
+
+		while (root != null) {
+			st.push(root);
+			root = root.left;
+		}
+
+		while (k != 0) {
+			TreeNode n = st.pop();
+			k--;
+			if (k == 0)
+				return n.val;
+			TreeNode right = n.right;
+			while (right != null) {
+				st.push(right);
+				right = right.left;
+			}
+		}
+		return -1; // never hit if k is valid
+	}
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
